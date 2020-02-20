8d6ca90724bc817cf7e1d7a0eb8b758c53bfc5e0
Gabriel Valencia <gvalenc@us.ibm.com>
2020-02-20 12:10:17 -0600

    fix: lookup landing page elems by tag name (#41)

--- scripts/python_django.py
+++ scripts/python_django.py
@@ -13,16 +13,24 @@ driver.get(os.environ["APP_URL"]); # Open a browser to the app's landing page
 time.sleep(3)
 
 # Verify the expected content is present
-title_text = driver.find_element_by_class_name('title')
+title_texts = driver.driver.find_elements_by_tag_name('h1')
+if len(title_texts) == 0:
+    sys.exit("Experience Test Failed: no title texts found")
+
+title_text = title_texts[0].text
 print("The title text is: {}".format(title_text))
 if title_text == "Congratulations!":
     print("Experience Test Successful")
 else:
-    sys.exit("Experience Test Failed")
+    sys.exit("Experience Test Failed: unexpected subtitle text {}".format(title_text))
 
-subtitle_text = driver.find_element_by_class_name('subtitle')
+subtitle_texts = driver.find_element_by_class_name('h2')
+if len(subtitle_texts) == 0:
+    sys.exit("Experience Test Failed: no subtitle texts found")
+
+subtitle_text = subtitle_texts[0].text
 print("The subtitle text is: {}".format(subtitle_text))
 if subtitle_text == "You are currently running a Django server.":
     print("Experience Test Successful")
 else:
-    sys.exit("Experience Test Failed")
+    sys.exit("Experience Test Failed: unexpected subtitle text {}".format(subtitle_text))

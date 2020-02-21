5b8c04a29778bc8f4582d46dc64e1fea9172c46e
Chuck Cox <chuckc@us.ibm.com>
2020-02-21 11:12:22 -0600

    Fix Selenium test (#46) (#47)
    * Update README.md
    
    * fix: find element
    
    * chore: dummy change to trigger monitoring
    
    * chore: dummy change to trigger monitoring (#40)
    
    * fix: lookup landing page elems by tag name (#41)
    
    * docs(readme): update logo src (#42)
    
    * fix: lookup landing page elems by tag name (#43)
    
    * chore: dummy change to trigger monitoring
    
    * fix: lookup landing page elems by tag name (#41)
    
    * docs(readme): update logo src (#42)
    
    Co-authored-by: Joseph Meis <jmeis@users.noreply.github.com>
    
    * fix: typo in driver call (#44)
    
    * fix: changed to xpath
    
    Results from my manual test:  
    ```
    Congratulations!
    You are currently running a Django server.
    The length of h1 is: 16
    The length of h2 is: 42
    ```
    
    * fix: var typo
    
    Co-authored-by: Joseph Meis <jmeis@users.noreply.github.com>
    Co-authored-by: Nick Steinhauser <nicksteinhauser@gmail.com>
    Co-authored-by: Gabriel Valencia <gee4vee@me.com>
    
    Co-authored-by: Joseph Meis <jmeis@users.noreply.github.com>
    Co-authored-by: Nick Steinhauser <nicksteinhauser@gmail.com>
    Co-authored-by: Gabriel Valencia <gee4vee@me.com>

--- scripts/python_django.py
+++ scripts/python_django.py
@@ -13,24 +13,22 @@ driver.get(os.environ["APP_URL"]); # Open a browser to the app's landing page
 time.sleep(3)
 
 # Verify the expected content is present
-title_texts = driver.find_elements_by_tag_name('h1')
-if len(title_texts) == 0:
+title_text = driver.find_elements_by_xpath('//h1[@class="title"]')[0].text
+if len(title_text) == 0:
     sys.exit("Experience Test Failed: no title texts found")
-
-title_text = title_texts[0].text
-print("The title text is: {}".format(title_text))
-if title_text == "Congratulations!":
-    print("Experience Test Successful")
 else:
-    sys.exit("Experience Test Failed: unexpected subtitle text {}".format(title_text))
+    print("The title text is: {}".format(title_text))
+    if title_text == "Congratulations!":
+        print("Experience Test Successful")
+    else:
+        sys.exit("Experience Test Failed: unexpected subtitle text {}".format(title_text))
 
-subtitle_texts = driver.find_element_by_class_name('h2')
-if len(subtitle_texts) == 0:
+subtitle_text = driver.find_elements_by_xpath('//h2[@class="subtitle"]')[0].text
+if len(subtitle_text) == 0:
     sys.exit("Experience Test Failed: no subtitle texts found")
-
-subtitle_text = subtitle_texts[0].text
-print("The subtitle text is: {}".format(subtitle_text))
-if subtitle_text == "You are currently running a Django server.":
-    print("Experience Test Successful")
 else:
-    sys.exit("Experience Test Failed: unexpected subtitle text {}".format(subtitle_text))
+    print("The subtitle text is: {}".format(subtitle_text))
+    if subtitle_text == "You are currently running a Django server.":
+        print("Experience Test Successful")
+    else:
+        sys.exit("Experience Test Failed: unexpected subtitle text {}".format(subtitle_text))

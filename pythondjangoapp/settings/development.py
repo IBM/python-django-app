2fdad2c039c5b6cd8abab202d087c7baf844477b
Chuck Cox <chuckc@us.ibm.com>
2020-06-09 17:45:00 -0500

    fix: linting error

---
+++ pythondjangoapp/settings/development.py
@@ -0,0 +1,7 @@
+from pythondjangoapp.settings.base import *
+
+DEBUG = True
+
+INSTALLED_APPS += (
+                   # other apps for local development
+                   )
---
+++ pythondjangoapp/settings/production.py
@@ -0,0 +1,7 @@
+from pythondjangoapp.settings.base import *
+
+DEBUG = False
+
+INSTALLED_APPS += (
+                   # other apps for production
+                   )
